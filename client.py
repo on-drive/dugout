082a4671935a8580fa5c56de7df1c88dac74c7c7
Abhinav kumar <abhinav72610@gmail.com>
2021-05-22 16:02:41 +0530

    converted broadcast message to binary from string

--- client.py
+++ client.py
@@ -1,22 +1,21 @@
-# Python program to implement client side of chat room.
 import socket
 import select
 import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 2:
-	print ("Correct usage: script, IP address, port number")
-	exit()
+    print("Correct usage: script, IP address, port number")
+    exit()
 IP_address = socket.gethostbyname(socket.gethostname())
 Port = int(sys.argv[1])
 server.connect((IP_address, Port))
 
 while True:
 
-	# maintains a list of possible input streams
-	sockets_list = [sys.stdin, server]
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
 
-	""" There are two possible input situations. Either the
+    """ There are two possible input situations. Either the
 	user wants to give manual input to send to other people,
 	or the server is sending a message to be printed on the
 	screen. Select returns from sockets_list, the stream that
@@ -24,16 +23,18 @@ while True:
 	to send a message, then the if condition will hold true
 	below.If the user wants to send a message, the else
 	condition will evaluate as true"""
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    read_sockets, write_socket, error_socket = select.select(
+        sockets_list, [], [])
 
-	for socks in read_sockets:
-		if socks == server:
-			message = socks.recv(2048)
-			print (message)
-		else:
-			message = sys.stdin.readline()
-			server.send(message)
-			sys.stdout.write("<You>")
-			sys.stdout.write(message)
-			sys.stdout.flush()
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            message1 = bytes(message, 'utf-8')
+            server.send(message1)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
 server.close()
