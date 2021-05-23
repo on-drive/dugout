9e6d780ff99b83d1353fe6edf801dd0efd9dc403
ooyeraju <rajnishkush2@gmail.com>
2021-05-23 18:25:08 +0530

    inspired by gfg

--- client.py
+++ client.py
@@ -1,33 +1,39 @@
 import socket
+import select
+import sys
 
 HEADER = 1024
 PORT = 5050
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.55.221"
-ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+# DISCONNECT_MESSAGE = "!DISCONNECT"
+IP_ADD = "192.168.55.221"
+ADDR = (IP_ADD, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
-    message = msg.encode(FORMAT)
-    # msg_length = len(message)
-    # send_length = str(msg_length).encode(FORMAT)
-    # send_length += b' ' * (HEADER - len(send_length))
-    # client.send(send_length)
-    client.send(message)
-    # rec_add = client.recv(HEADER).decode(FORMAT)
-    rec_message = client.recv(HEADER).decode(FORMAT)
-    print(  ADDR +rec_message )
+# def send(msg):
+#     message = msg.encode(FORMAT)
+#     # msg_length = len(message)
+#     # send_length = str(msg_length).encode(FORMAT)
+#     # send_length += b' ' * (HEADER - len(send_length))
+#     # client.send(send_length)
+#     client.send(message)
+#     # rec_add = client.recv(HEADER).decode(FORMAT)
 
 
+while True:
+    sockets_list = [sys.stdin, client]
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
 
-while True: 
-    user_message = input()
-    send(user_message)
-
-recv_msg = client.recv(HEADER).decode(FORMAT)
-# print(recv_msg)
-
-send(DISCONNECT_MESSAGE)
+    for socks in read_sockets:
+        if socks == client:
+            message = socks.recv(HEADER)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            client.send(message)
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+client.close()
--- server.py
+++ server.py
@@ -1,40 +1,98 @@
-import socket 
+import socket
 import threading
+import _thread
+from _thread import start_new_thread
 
 HEADER = 1024
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+IP_ADD = socket.gethostbyname(socket.gethostname())
+ADDR = (IP_ADD, PORT)
+FORMAT = "utf-8"
+# DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+server.listen(5)
+
+clients_list = []
+
+
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
+    # print(f"[NEW CONNECTION] {addr} connected.")
+    conn.send("Welcome to baba ka dugout")
     connected = True
-    while connected:
-        # msg_length = conn.recv(HEADER).decode(FORMAT)
-        # if msg_length:
-        #     msg_length = int(msg_length)
-        msg = conn.recv(HEADER).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        conn.send(msg.encode(FORMAT))
+    # while connected:
+    #     # msg_length = conn.recv(HEADER).decode(FORMAT)
+    #     # if msg_length:
+    #     #     msg_length = int(msg_length)
+    #     msg = conn.recv(HEADER).decode(FORMAT)
+    #     if msg == DISCONNECT_MESSAGE:
+    #         connected = False
+    #     conn.send(msg.encode(FORMAT))
 
-    conn.close()   
-
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        try:
+            msg = conn.recv(HEADER)
+            if msg:
+                print(f"[{addr} ] - {msg}")
+                message_to_send = f"[{addr}] - {msg}"
+                broadcast(message_to_send, conn)
+            else:
+                remove(conn)
+        except:
+            continue
 
 
-print("[STARTING] server is starting...")
-start()
+def broadcast(message, connection):
+    for clients in clients_list:
+        if clients != connection:
+            try:
+                clients.send(message)
+            except:
+                clients.close()
+                # if the link is broken, we remove the client
+                remove(clients)
+
+
+def remove(connection):
+    if connection in clients_list:
+        clients_list.remove(connection)
+
+
+# def start():
+#     server.listen()
+#     print(f"[LISTENING] Server is listening on {SERVER}")
+#     while True:
+#         conn, addr = server.accept()
+#         clients_list.append(conn)
+#         thread = threading.Thread(target=handle_client, args=(conn, addr))
+#         thread.start()
+#         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+# print("[STARTING] server is starting...")
+# start()
+
+
+while True:
+
+    """Accepts a connection request and stores two parameters,
+    conn which is a socket object for that user, and addr
+    which contains the IP address of the client that just
+    connected"""
+    conn, addr = server.accept()
+
+    """Maintains a list of clients for ease of broadcasting 
+    a message to all available people in the chatroom"""
+    clients_list.append(conn)
+
+    # prints the address of the user that just connected
+    # print(addr[0] + " connected")
+
+    # creates and individual thread for every user
+    # that connects
+    start_new_thread(handle_client, (conn, addr))
+
+conn.close()
+server.close()
